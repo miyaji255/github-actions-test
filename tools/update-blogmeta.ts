0ecd0e4c8e2d77127cdb9a6346bdc34250201b5c
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2023-12-12 16:19:34 +0900

    update ts

--- tools/update-blogmeta.ts
+++ tools/update-blogmeta.ts
@@ -1,171 +1,63 @@
-import * as fs from "fs";
 import * as path from "path";
 
-type FileStatus =
-  | [status: "A" | "C" | "D" | "M" | "T", path: string]
-  | [status: `R${number}`, fromPath: string, toPath: string];
-
-type BlogMeta = {
-  postDate: string;
-  updateDate?: string;
-};
-const gitDiffs = validateArgs(chunkArray(process.argv.slice(2), 2));
-if (gitDiffs === null) process.exit(1);
-updateBlogMeta(gitDiffs);
-
-function validateArgs(gitDiffs: string[][]): FileStatus[] | null {
-  let ok = true;
-  function validateFile(path: string) {
-    if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
-      console.log(`\u001b[31m[ERROR]\tThe file was not found: ${path}\u001b[m`);
-      ok = false;
-      return false;
-    }
-    return true;
-  }
-
-  const result: FileStatus[] = [];
-
-  for (const status of gitDiffs) {
-    if (status.length != 2 && status.length != 3) {
-      console.log(
-        `\u001b[31m[ERROR]\tArgument is invalid format.\n Please set results of \`git diff --name-status\`. \u001b[m`
-      );
-      ok = false;
-      continue;
-    }
-
-    if (status.length === 2) {
-      if (!["A", "C", "D", "M", "T"].includes(status[0]!)) {
-        console.log(
-          `\u001b[33m[WARNING]\tThe status does not supported: ${status[0]}\t${status[1]}\u001b[m`
-        );
-        continue;
-      }
-
-      if (status[0] === "D" || validateFile(status[1]!)) {
-        result.push(status as ["A" | "C" | "D" | "M" | "T", string]);
-      }
-    } else if (status.length === 3) {
-      if (
-        status[0]!.startsWith("R") &&
-        Number.isInteger(Number(status[0]!.slice(1)))
-      ) {
-        if (validateFile(status[1]!) && validateFile(status[2]!)) {
-          result.push(status as [`R${number}`, string, string]);
-        }
-      } else
-        console.log(
-          `\u001b[33m[WARNING]\tThe status does not supported: ${status[0]}\t${status[1]}\u001b[m`
-        );
-    }
-  }
-  return ok ? result : null;
-}
+if (!validateBlog()) process.exit(1);
+console.log("\u001b[32m[INFO]\tThe validation was passed.\u001b[m");
 
 /**
- * blog meta のファイルの日付を更新します
+ * 変更されたファイルが許可されているものに含まれているか判定します
+ *
+ * 許可されているファイルは以下のとおりです。
+ * - src/content/blogs/*.{md, mdx}
+ * - src/content/blogs/ より一階層下にある画像
+ * - src/content/authors/*.json
+ * - src/content/authors/ にある画像
+ * - src/content/blog-metas/*.json
+ * - src/content/tags/*.json
+ * - src/content/tags/ にある画像
  */
-function updateBlogMeta(gitDiffs: readonly FileStatus[]) {
-  const targetFiles = gitDiffs
-    .map(([status, filePath, toPath]) =>
-      toPath === undefined
-        ? ([status, path.parse(filePath)] as const)
-        : ([status, path.parse(toPath)] as const)
-    )
-    .filter(predicateTargetFiles);
-  const updatedMetas = gitDiffs
-    .map(([status, filePath, toPath]) => [status, toPath ?? filePath] as const)
-    .filter(([status, filePath]) => {
-      const { dir, ext } = path.parse(filePath);
-      return (
-        status !== "D" && dir === "src/content/blog-metas" && ext === ".json"
-      );
-    });
+function validateBlog() {
+  let ok = true;
 
-  for (const [_, { dir, name, ext }] of targetFiles) {
-    const updatedName =
-      ext === ".md" || ext === ".mdx"
-        ? name
-        : dir.slice("src/content/blogs/".length).split("/")[0]!;
+  for (const arg of process.argv.slice(2)) {
+    const parsedPath = path.parse(arg);
 
-    const blogMetaPath = `src/content/blog-metas/${updatedName}.json`;
-
-    let meta: BlogMeta | null = null;
-    if (fs.existsSync(blogMetaPath)) {
-      meta = JSON.parse(
-        fs.readFileSync(blogMetaPath, { encoding: "utf-8" })
-      ) as BlogMeta;
-
-      if (updatedMetas.every(([_, filePath]) => filePath !== blogMetaPath))
-        meta.updateDate = new Date().toISOString();
-      else {
-        // 今回のPRで既に更新済み
-
-        if (meta.updateDate) meta.updateDate = new Date().toISOString();
-        else meta.postDate = new Date().toISOString();
-      }
-    } else {
-      meta = {
-        postDate: new Date().toISOString(),
-      };
+    if (parsedPath.dir === "src/content/blogs") {
+      // ブログ本体
+      if (parsedPath.ext === ".md" || parsedPath.ext === ".mdx") continue;
+    } else if (parsedPath.dir === "src/content/blog-metas") {
+      // ブログのメタ情報
+      if (parsedPath.ext === ".json") continue;
+    } else if (parsedPath.dir.match(/^src\/content\/blogs\/[^(#/)]*$/)) {
+      // 画像
+      if (isImage(parsedPath.ext)) continue;
+    } else if (
+      ["src/content/authors", "src/content/tags"].includes(parsedPath.dir)
+    ) {
+      // ブログの著者・タグ
+      if (parsedPath.ext === ".json" || isImage(parsedPath.ext)) continue;
     }
 
-    fs.writeFileSync(blogMetaPath, JSON.stringify(meta, undefined, 2) + "\n");
-
-    console.log(`[INFO]\tUpdate blog meta: ${blogMetaPath}`);
+    console.log(
+      `\u001b[31m[ERROR]\tYou are not allowed to change the file: ${arg}\u001b[m`
+    );
+    ok = false;
   }
+
+  return ok;
 }
 
-function predicateTargetFiles([status, file]: readonly [
-  "A" | "C" | "D" | "M" | "T" | `R${number}`,
-  path.ParsedPath
-]): boolean {
-  // markdown
-  if (
-    (["A", "C", "M", "T"].includes(status) || status.startsWith("R")) &&
-    file.dir === "src/content/blogs" &&
-    (file.ext === ".md" || file.ext === ".mdx")
-  )
-    return true;
-
-  // 画像
-  if (
-    (["A", "C", "D", "M", "T"].includes(status) || status.startsWith("R")) &&
-    file.dir.startsWith("src/content/blogs/") &&
-    [
-      ".jpg",
-      ".jpeg",
-      ".jfif",
-      ".pjpeg",
-      ".pjp",
-      ".png",
-      ".svg",
-      ".webp",
-      ".gif",
-      ".avif",
-      ".apng",
-    ].includes(file.ext)
-  )
-    return true;
-
-  return false;
-}
-
-function chunkArray<T>(array: T[], chunkSize: number) {
-  const result: T[][] = [];
-  let tempArray: T[] = [];
-  for (const val of array) {
-    if (tempArray.length < chunkSize) {
-      tempArray.push(val);
-    } else {
-      result.push(tempArray);
-      tempArray = [val];
-    }
-  }
-
-  if (tempArray.length !== 0) {
-    result.push(tempArray);
-  }
-  return result;
+function isImage(ext: string) {
+  return [
+    ".jpg",
+    ".jpeg",
+    ".jfif",
+    ".pjpeg",
+    ".pjp",
+    ".png",
+    ".svg",
+    ".webp",
+    ".gif",
+    ".avif",
+    ".apng",
+  ].includes(ext);
 }
--- tools/validate-blog.ts
+++ tools/validate-blog.ts
@@ -1,6 +1,6 @@
-import * as path from 'path'
+import * as path from "path";
 
-process.exit(validateBlog() ? 0 : 1)
+process.exit(validateBlog() ? 0 : 1);
 
 /**
  * 変更されたファイルが許可されているものに含まれているか判定します
@@ -15,50 +15,50 @@ process.exit(validateBlog() ? 0 : 1)
  * - src/content/tags/ にある画像
  */
 function validateBlog() {
-  let ok = true
+  let ok = true;
 
   for (const arg of process.argv.slice(2)) {
-    const parsedPath = path.parse(arg)
+    const parsedPath = path.parse(arg);
 
-    if (parsedPath.dir === 'src/content/blogs') {
+    if (parsedPath.dir === "src/content/blogs") {
       // ブログ本体
-      if (parsedPath.ext === '.md' || parsedPath.ext === '.mdx') continue
-    } else if (parsedPath.dir === 'src/content/blog-metas') {
+      if (parsedPath.ext === ".md" || parsedPath.ext === ".mdx") continue;
+    } else if (parsedPath.dir === "src/content/blog-metas") {
       // ブログのメタ情報
-      if (parsedPath.ext === '.json') continue
+      if (parsedPath.ext === ".json") continue;
     } else if (
-      parsedPath.dir.match(new RegExp('src/content/blogs/[!(#/)]*/[!(#/)]*'))
+      parsedPath.dir.match(/^src\/content\/blogs\/[^(#/)]*\/[^(#/)]*$/)
     ) {
       // 画像
-      if (isImage(parsedPath.ext)) continue
+      if (isImage(parsedPath.ext)) continue;
     } else if (
-      ['src/content/authors', 'src/content/tags'].includes(parsedPath.dir)
+      ["src/content/authors", "src/content/tags"].includes(parsedPath.dir)
     ) {
       // ブログの著者・タグ
-      if (parsedPath.ext === '.json' || isImage(parsedPath.ext)) continue
+      if (parsedPath.ext === ".json" || isImage(parsedPath.ext)) continue;
     }
 
     console.log(
-      `\u001b[31m[ERROR]\tYou are not allowed to change the file: ${arg}\u001b[m`,
-    )
-    ok = false
+      `\u001b[31m[ERROR]\tYou are not allowed to change the file: ${arg}\u001b[m`
+    );
+    ok = false;
   }
 
-  return ok
+  return ok;
 }
 
 function isImage(ext: string) {
   return [
-    '.jpg',
-    '.jpeg',
-    '.jfif',
-    '.pjpeg',
-    '.pjp',
-    '.png',
-    '.svg',
-    '.webp',
-    '.gif',
-    '.avif',
-    '.apng',
-  ].includes(ext)
+    ".jpg",
+    ".jpeg",
+    ".jfif",
+    ".pjpeg",
+    ".pjp",
+    ".png",
+    ".svg",
+    ".webp",
+    ".gif",
+    ".avif",
+    ".apng",
+  ].includes(ext);
 }
